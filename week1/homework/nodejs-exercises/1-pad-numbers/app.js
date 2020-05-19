6ff3c15c07117079698b70dc6dd3b9034fb899cf
Anis Alkomem <anis.alkumaim@gmail.com>
2020-05-19 13:39:46 +0200

    Finished node-exercises

---
+++ week1/homework/nodejs-exercises/1-pad-numbers/app.js
@@ -0,0 +1,5 @@
+const padLeft = require("./padLeft");
+
+const numbers = ["12", "846", "2", "1236"];
+
+numbers.forEach((number) => console.log(padLeft(number, 4, " ")));
---
+++ week1/homework/nodejs-exercises/3-web-server/server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+// create a server
+const server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    fs.readFile("./index.html", (err, content) => {
+      if (!err) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        // res.write(); // Sends a response back to the client
+        res.end(content); // Sends and ends the response
+      }
+    });
+  }
+  if (req.url === "/script.js") {
+    fs.readFile("./script.js", (err, content) => {
+      if (!err) {
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        // res.write(); // Sends a response back to the client
+        res.end(content); // Sends and ends the response
+      }
+    });
+  }
+  if (req.url === "/style.css") {
+    fs.readFile("./style.css", (err, content) => {
+      if (!err) {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        // res.write(); // Sends a response back to the client
+        res.end(content); // Sends and ends the response
+      }
+    });
+  }
+});
+
+server.listen(3000); // The server listens on port 3000
