47f2bb2cbff4e034c773abc572906972aae9ee6a
Anis Alkomem <anis.alkumaim@gmail.com>
2020-05-27 17:44:03 +0200

    Finished Project

--- week2/homework/hackyourtemperature/server.js
+++ week2/homework/hackyourtemperature/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const hbs = require("express-handlebars");
 
+// Initialize app
 const app = express();
-app.get("/", (req, res) => {
-  res.send("hello from backend to frontend!");
-});
+
+// Import the routes
+const weatherRoutes = require("./routes/weather");
+
+// Initialise handlebars engine
+app.engine("hbs", hbs({ defaultLayout: "main-layout", extname: "hbs" }));
+app.set("view engine", "hbs");
+
+// Use express parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use routes
+app.use(weatherRoutes);
 
 const PORT = process.env.port || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} `));
