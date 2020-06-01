bc4956f464e1db32c26c2a3c167dcc17c762c98b
Anis Alkomem <anis.alkumaim@gmail.com>
2020-06-01 20:01:20 +0200

    Brought project files from week 2

---
+++ week3/homework/hackyourtemperature/routes/weather.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { pageTitle: "HackYourTemperature App" });
+});
+router.post("/weather", (req, res) => {
+  const { cityName } = req.body;
+
+  res.end(cityName);
+});
+
+module.exports = router;
